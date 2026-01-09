a5252e77cd9cebf5a8568136fdc3f1d8875821cc
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2026-01-09 09:59:51 +0100

    Update script.js

--- zwa/12-12-25/script.js
+++ zwa/12-12-25/script.js
@@ -1,33 +1,41 @@
 const datum = document.getElementById('datum')
 const table = document.getElementById('table')
 
+const dnyNaMesic = [31,28,31,30,30,31,31,30,31,30,31]
 const TYDEN = ['nedele','pondeli', 'utery', 'streda', 'ctvrtek', 'patek', 'sobota'] 
 
 function generovat(){
 
-    let date = new Date(`${datum.value.substring(0,8)}01`);
-    const den = date.getDay();
+    let date = new Date(`${datum.value.substring(0,8)}01`)
+    const den = date.getDay()
+
+    let pocetDni = dnyNaMesic[date.getMonth()]
+    if(date.getFullYear()%4 == 0 && date.getMonth()+1 == 2) pocetDni++
+    console.log(date.getFullYear(), date.getMonth())
 
     let counter = 0
-    for(let y = 0; y< 5; y++){
+    for(let y = 0; y< 6; y++){
 
         let row = document.createElement('tr')
 
         for(let x = 0; x<7; x++){
             
-            let cell = document.createElement('td')
-                
+            let cell
             if(y==0){
+                cell = document.createElement('td')
                 cell.innerText = TYDEN[x]
                 cell.style.backgroundColor = 'black'
-                
-            }else if((x >= den && y == 1) || y>1){
+                row.appendChild(cell)
+
+            }else if(((x >= den && y == 1) || y>1) && counter < pocetDni){
+                cell = document.createElement('td')
+
                 counter++
                 cell.innerText = counter
                 
                 if(datum.value[8] + datum.value[9] == counter) cell.style.backgroundColor = 'rgb(80,0,0)'
+                row.appendChild(cell)
             }
-            row.appendChild(cell)
         }
         table.appendChild(row)
     }
