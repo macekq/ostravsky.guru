9b21c3b42a6eae7bd6f75edd50512e575b11962e
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2026-01-15 18:43:55 +0100

    kalendar fixed

--- zwa/kalendar/script.js
+++ zwa/kalendar/script.js
@@ -9,33 +9,34 @@ function generovat(){
     let date = new Date(`${datum.value.substring(0,8)}01`)
     const den = date.getDay()
 
+
     let pocetDni = dnyNaMesic[date.getMonth()]
     if(date.getFullYear()%4 == 0 && date.getMonth()+1 == 2) pocetDni++
-    console.log(date.getFullYear(), date.getMonth())
-
-    let counter = 0
-    for(let y = 0; y< 6; y++){
+    
+    let counter = 0, opakovani = Math.ceil((den+pocetDni)/7);
+    console.log(opakovani)
+    for(let y = 0; y <= opakovani; y++){
 
         let row = document.createElement('tr')
 
         for(let x = 0; x<7; x++){
-            
             let cell
+            cell = document.createElement('td')
             if(y==0){
-                cell = document.createElement('td')
+                
                 cell.innerText = TYDEN[x]
-                cell.style.backgroundColor = 'black'
-                row.appendChild(cell)
+                cell.style.backgroundColor = '#3D1FA5'
 
             }else if(((x >= den && y == 1) || y>1) && counter < pocetDni){
-                cell = document.createElement('td')
-
+                
                 counter++
                 cell.innerText = counter
                 
                 if(datum.value[8] + datum.value[9] == counter) cell.style.backgroundColor = 'rgb(80,0,0)'
-                row.appendChild(cell)
+            }else{
+                cell.style.backgroundColor = 'rgb(32,32,32)'
             }
+            row.appendChild(cell)
         }
         table.appendChild(row)
     }
