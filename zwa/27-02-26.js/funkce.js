1b74596a3111242363cfd7ddd3a3a61279c51d5c
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2026-02-28 15:00:09 +0100

    a

--- zwa/27-02-26.js/funkce.js
+++ zwa/27-02-26.js/funkce.js
@@ -1,8 +1,6 @@
-let region = document.getElementById("region")
-
+const region = document.getElementById("region")
 let regiony = [];
-
-let regionyBtt = document.getElementById("nacistRegiony")
+const regionyBtt = document.getElementById("nacistRegiony")
 
 regionyBtt.addEventListener('click', () => {
 		
@@ -12,8 +10,7 @@ regionyBtt.addEventListener('click', () => {
 	const request = new XMLHttpRequest();
 		request.onload = function() {
 			data = JSON.parse(this.responseText);
-			
-			// console.log(data);
+
 			let opt = document.createElement("option");
 			
 			opt.value = "-";
@@ -21,13 +18,12 @@ regionyBtt.addEventListener('click', () => {
 			
 			region.innerHTML = "";
 			region.append(opt);
-			
 
 			Object.values(data).forEach((hodnota) => {
 				
-				console.log(hodnota);
-				console.log(hodnota["region"]);
-				console.log(regiony.indexOf(hodnota["region"]));
+				// console.log(hodnota);
+				// console.log(hodnota["region"]);
+				// console.log(regiony.indexOf(hodnota["region"]));
 				
 				if (regiony.indexOf(hodnota["region"]) < 0) {
 				
@@ -42,68 +38,126 @@ regionyBtt.addEventListener('click', () => {
 		request.open("GET", url);
 		request.send();
 })
-let div = document.getElementById('cont')
+const div = document.getElementById('cont'); info = document.getElementById('coutryInfo')
+const table = document.getElementById('table')
 
 region.addEventListener('change', () => {
+	table.innerHTML = ''
+	try{
+		document.getElementById('select2').remove()
+	}catch(error){}
 
-	let url = "https://restcountries.com/v3.1/all?fields=region,name,currencies,unMember,regionalBlocs"
-
-	// window.alert('true')
-	window.alert(region.value)
+	let url = "https://restcountries.com/v3.1/all?fields=region,name,currencies,unMember,subregion,capital,population,flag,borders"
 
 	let select = document.createElement('select')
 	select.id = 'select2'
-	
-	let mena = [];
+
+	let unCounter = 0;
 
 	const request = new XMLHttpRequest();
 	request.onload = function() {
 		
-		
+		let last = '';
 		let data = JSON.parse(this.responseText);
 		let statyVRegionu = data.filter(stat => stat.region === region.value);
 		
 		let nullOpt = document.createElement('option')
 		nullOpt.value = -1; nullOpt.innerText = '-------'
 		select.appendChild(nullOpt)
+		
+		
+		let stRow = document.createElement('tr')
+		stRow.style.backgroundColor = 'black'
+		stRow.style.color = 'white'
 
-		Object.entries(mena).forEach(([klic, nazev]) => {
-			console.log(klic, nazev)
-		});
+		let coutryCount = document.createElement('td')
+		coutryCount.innerText = `celkovy pocet: ${statyVRegionu.length}`
+		let unCount = document.createElement('td')
+		stRow.append(coutryCount, document.createElement('td'), unCount)
+		table.appendChild(stRow)
 
-		let index = 0;
-		statyVRegionu.forEach((hodnota) => {
-			console.log(hodnota["name"]['common'], data[index]["currencies"])
-			mena.push([data[index]["currencies"]])
 
+		// console.log(statyVRegionu)
+		for(let i = 0; i<statyVRegionu.length; i++){
+			// console.log(statyVRegionu[i])
+			
+			//vytvoreni tabulky pro zobrazeni podragionu
+
+			let row = document.createElement('tr')
+			row.id = `row${i}`
+
+			let regionCell = document.createElement('td')
+			regionCell.innerText = statyVRegionu[i].name.official
+
+			let menaCell = document.createElement('td')
+			let first = true; text = '';
+			Object.entries(statyVRegionu[i].currencies).forEach(([klic, obj]) => {
+					
+				if(first){
+					first = false
+					text += 'hlavni '
+				}
+				text += `mena: ${klic} ${obj.name} - ${obj.symbol}\n\n`
+			})
+			menaCell.innerText = text
+			let un = document.createElement('td')
+
+			un.innerText = statyVRegionu[i].unMember ? 'ano' : 'ne'
+			unCounter = statyVRegionu[i].unMember ? unCounter+1 : unCounter
+			
+			row.append(regionCell, menaCell, un)
+			table.appendChild(row)
+
+			//vyplneni selectu2 -> vyber podregionu/statu
 			let opt = document.createElement('option')
 			
-			opt.innerText = hodnota["name"]['common']
-			opt.value = index
-			
-			// console.log(hodnota);
-			// console.log(hodnota["name"]["common"]);
-			
+			opt.innerText = statyVRegionu[i].name.common
+			opt.value = i
+
 			select.appendChild(opt)
 			
-			
-			index++
-		})
+			//po vybrani ze selectu 2 se zobrazi info o statu
+			opt.addEventListener('click', () => {
+				
+				try{document.getElementById(last).style.backgroundColor = ''}catch(error){}
+				document.getElementById(`row${opt.value}`).style.backgroundColor = 'red'
+				last = `row${opt.value}`
+
+				let text = `oficialni nazev: ${statyVRegionu[i].name.official} [${statyVRegionu[i].name.official.substring(0, 3).toUpperCase()}]\n\n`
+				let first = true
+				info.innerHTML = ''
+				
+				console.log(statyVRegionu[i])
+
+				Object.entries(statyVRegionu[i].currencies).forEach(([klic, obj]) => {
+					
+					if(first){
+						first = false
+						text += 'hlavni '
+					}
+					text += `mena: ${klic} ${obj.name} - ${obj.symbol}\n`
+
+				})
+				text += `hlavni mesto: ${statyVRegionu[i].capital[0]}`
+				text += `\nun member: ${statyVRegionu[i].unMember ? 'ano' : 'ne'}`
+				text += `\npopulace: ${statyVRegionu[i].population}`
+				text += `\nvlajka: ${statyVRegionu[i].flag}`
+				text += `\npodregion: ${statyVRegionu[i].subregion}`
+				text += `\n\nsousedici zeme:`
+				for(let item of statyVRegionu[i].borders){
+					text += '\n-> ' + item
+				}
+				// console.log(statyVRegionu[i].flag, typeof statyVRegionu[i].flag)
+				
+				const coutryInfo = document.createElement('div')
+				coutryInfo.innerText = text
+				info.appendChild(coutryInfo)
+			})
+		}
+		unCount.innerText = unCounter
 	};
 	request.open("GET", url);
 	request.send();
-	
+
 	div.appendChild(select)
-
-	console.log(mena)
-	
-	select.addEventListener('change', () => {
-		console.log(mena[select.value])
-
-		let text = document.createElement('div')
-		text.innerText = `statni mena: ${mena[select.value]['ZWL']['name']} - ${mena[select.value]['ZWL']['symbol']}`
-
-		div.appendChild(text)
-	})
-
 })
